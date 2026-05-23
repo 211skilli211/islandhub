7f3d55b474933359c4dd0beebf954d4cb8cee74d
root <root@localhost.localdomain>
2026-05-23 17:41:02 +0000

    Add animation components: ScrollReveal, TiltCard, StaggerContainer, FadeIn, ScaleIn

---
+++ web/src/components/shaders/index.ts
@@ -0,0 +1,4 @@
+export { default as ShaderBackground } from './ShaderBackground';
+export { default as ParticleField } from './ParticleField';
+export { SHADERS, getShaderNames } from './shaderRegistry';
+export type { ShaderConfig } from './shaderRegistry';
